554f6a695fd488c59e1751f5c3f506c87dacc1e2
Elena Plakhotnaya <lena.9.12@yandex.ru>
2024-05-21 21:39:53 +0300

    1

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,44 @@
+# birthday/forms.py
+from django import forms
+from django.db import models
+# Импортируем класс модели Birthday.
+from .models import Birthday
+from django.core.exceptions import ValidationError
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+# Для использования формы с моделями меняем класс на forms.ModelForm.
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+    
+    def clean(self):
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
+         
